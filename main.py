f5845edf0a3812624868110a01662c3e2b4c56cb
FightlolYes <yashpatil.uwu@gmail.com>
2022-09-04 16:05:12 +0530

    testing around matplotlib and numpy

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+
 no = int(input("Enter a number: "))
 
 while no != 1:
@@ -8,3 +12,9 @@ while no != 1:
         no = (no * 3) + 1
 
     print(int(no))
+
+x=  [1, 2, 3]
+y = [1, 4, 9]
+
+plt.plot(x, y)
+plt.show()
