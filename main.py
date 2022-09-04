c47457f2af2243317520f9deab0acf3f432070e4
FightlolYes <yashpatil.uwu@gmail.com>
2022-09-04 20:28:09 +0530

    added matplotlib for making graphs of the values

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas
+import pandas 
+
+def length(list):
+    count = 0
+    for element in list:
+        count += 1
+    return count
 
 no = int(input("Enter a number: "))
 
+graph = []
+
 while no != 1:
-    if no % 2 == 0:
+    if no%2 == 0:
         no = no/2
+        graph.append(int(no))
 
     else:
         no = (no * 3) + 1
+        graph.append(int(no))
 
     print(int(no))
 
-x=  [1, 2, 3]
-y = [1, 4, 9]
+print(graph)
+
+x = range(1, length(graph)+ 1)
+y = graph
 
 plt.plot(x, y)
 plt.show()
